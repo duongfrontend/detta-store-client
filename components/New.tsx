f44167b1e6ebb410b8ab995720c7f5405796e678
duongfrontend <phuongnguyen12a6@gmail.com>
2024-03-25 11:39:16 +0700

    ok

--- components/New.tsx
+++ components/New.tsx
@@ -1,4 +1,3 @@
-import { datas } from "@/datas";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,17 +5,36 @@ import React from "react";
 const New = () => {
   return (
     <div className="flex justify-center items-center gap-16 mt-[50px] w-[1600px] mx-[auto] mt-[3%] max-[1366px]:flex-col max-[1366px]:w-full max-[1366px]:gap-0">
-      {datas.map((item) => (
-        <div key={item.id}>
-          <Link href={item.link}>
-            <Image
-              className="rounded-[10px] animation-img"
-              src={item.image}
-              alt={""}
-            />
-          </Link>
-        </div>
-      ))}
+      <div>
+        <Link
+          href={`${process.env.NEXT_URL_NEW}/collections/65faa6dc1782038590df0e52`}>
+          <Image
+            className="rounded-[10px] animation-img"
+            src="/2.webp"
+            alt={""}
+          />
+        </Link>
+      </div>
+      <div>
+        <Link
+          href={`${process.env.NEXT_URL_NEW}/collections/65faa6581782038590df0e4d`}>
+          <Image
+            className="rounded-[10px] animation-img"
+            src="/3.webp"
+            alt={""}
+          />
+        </Link>
+      </div>
+      <div>
+        <Link
+          href={`${process.env.NEXT_URL_NEW}/collections/65faa6f51782038590df0e58`}>
+          <Image
+            className="rounded-[10px] animation-img"
+            src="/1.jpg"
+            alt={""}
+          />
+        </Link>
+      </div>
     </div>
   );
 };
