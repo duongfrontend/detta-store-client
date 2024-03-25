be420c9a7aaaee5ac28010cfdb78d80770e547f1
duongfrontend <phuongnguyen12a6@gmail.com>
2024-03-25 12:05:45 +0700

    okee

--- components/Outfilt.tsx
+++ components/Outfilt.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Outfilt = () => {
   return (
     <div className="w-full max-[664px]:gap-10 max-[664px]:flex-col max-[664px]:flex">
-      <div className="flex justify-center items-center bg-[#eae9e7] max-[664px]:flex-col max-[664px]:gap-10">
+      <div className="flex justify-center items-center bg-[#eae9e7] max-[664px]:flex-col max-[664px]:gap-10 max-[664px]:p-[10px]">
         <div className="flex-1">
           <img className="w-full" src="/6.webp" alt="áo" />
         </div>
@@ -28,7 +28,7 @@ const Outfilt = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center bg-[#eae9e7] max-[664px]:flex-col-reverse max-[664px]:gap-10">
+      <div className="flex justify-center items-center bg-[#eae9e7] max-[664px]:flex-col-reverse max-[664px]:gap-10 max-[664px]:p-[10px]">
         <div className="h-full flex-1 max-[664px]:w-full">
           <div className="ml-[8%] max-[1023px]:mr-[8%] max-[1023px]:ml-[0]">
             <h1 className="uppercase text-heading1-bold max-[1023px]:text-[25px] max-[664px]:text-[18px]">
